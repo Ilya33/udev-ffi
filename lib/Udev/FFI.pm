7cb67b5d68ca79f3bdf786e262ab30c5245091ac
Ilya Pavlov <iluxz@mail.ru>
2017-06-27 19:48:39 +0300

    pre-alpha

--- lib/Udev/FFI.pm
+++ lib/Udev/FFI.pm
@@ -100,3 +100,54 @@ sub DESTROY {
 
 
 1;
+
+
+
+__END__
+
+
+
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Udev::FFI - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Udev::FFI;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Udev::FFI.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Ilya Pavlov, E<lt>iluxz@mail.ru<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2017 by Ilya Pavlov
+
+
+=cut
