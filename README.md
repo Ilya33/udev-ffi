278d8ad215928d23d20ec4323c8916a1edca0171
Ilya <ilya@orz1e1.mydomain>
2017-06-27 18:55:04 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Udev::FFI - It's new $module
+
+# SYNOPSIS
+
+    use Udev::FFI;
+
+# DESCRIPTION
+
+Udev::FFI is ...
+
+# LICENSE
+
+Copyright (C) Ilya.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ilya <>
---
+++ lib/Udev/FFI.pm
@@ -0,0 +1,39 @@
+package Udev::FFI;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Udev::FFI - It's new $module
+
+=head1 SYNOPSIS
+
+    use Udev::FFI;
+
+=head1 DESCRIPTION
+
+Udev::FFI is ...
+
+=head1 LICENSE
+
+Copyright (C) Ilya.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ilya E<lt>E<gt>
+
+=cut
+
