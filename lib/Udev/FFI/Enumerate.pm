bff7b598d661aa4acb851adfc3b5b270b3c9edba
Ilya Pavlov <ilux@cpan.org>
2019-02-04 00:57:21 +0300

    improve error handling

--- lib/Udev/FFI/Enumerate.pm
+++ lib/Udev/FFI/Enumerate.pm
@@ -51,123 +51,113 @@ sub add_nomatch_subsystem {
 
 
 sub add_match_sysattr {
-    my $self = shift;
-    my $sysattr = shift;
-    my $value = shift;
-
-    if(0 != udev_enumerate_add_match_sysattr($self->{_enumerate}, $sysattr, $value)) {
-        return 0;
+    # self, sysattr, value
+    if (0 == ($! = udev_enumerate_add_match_sysattr($_[0]->{_enumerate}, $_[1], $_[2]))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub add_nomatch_sysattr {
-    my $self = shift;
-    my $sysattr = shift;
-    my $value = shift;
-
-    if(0 != udev_enumerate_add_nomatch_sysattr($self->{_enumerate}, $sysattr, $value)) {
-        return 0;
+    # self, sysattr, value
+    if (0 == ($! = udev_enumerate_add_nomatch_sysattr($_[0]->{_enumerate}, $_[1], $_[2]))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub add_match_property {
-    my $self = shift;
-    my $property = shift;
-    my $value = shift;
-
-    if(0 != udev_enumerate_add_match_property($self->{_enumerate}, $property, $value)) {
-        return 0;
+    # self, property, value
+    if (0 == ($! = udev_enumerate_add_match_property($_[0]->{_enumerate}, $_[1], $_[2]))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub add_match_sysname {
-    my $self = shift;
-    my $sysname = shift;
-
-    if(0 != udev_enumerate_add_match_sysname($self->{_enumerate}, $sysname)) {
-        return 0;
+    # self, sysname
+    if (0 == ($! = udev_enumerate_add_match_sysname($_[0]->{_enumerate}, $_[1]))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub add_match_tag {
-    my $self = shift;
-    my $tag = shift;
-
-    if(0 != udev_enumerate_add_match_tag($self->{_enumerate}, $tag)) {
-        return 0;
+    # self, tag
+    if (0 == ($! = udev_enumerate_add_match_tag($_[0]->{_enumerate}, $_[1]))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub add_match_parent {
-    my $self = shift;
-    my $parent = shift;
-
-    if(0 != udev_enumerate_add_match_parent($self->{_enumerate}, $parent)) {
-        return 0;
+    # self, parent
+    if (0 == ($! = udev_enumerate_add_match_parent($_[0]->{_enumerate}, $_[1]))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub add_match_is_initialized {
-    my $self = shift;
-
-    if(0 != udev_enumerate_add_match_is_initialized($self->{_enumerate})) {
-        return 0;
+    # self
+    if (0 == ($! = udev_enumerate_add_match_is_initialized($_[0]->{_enumerate}))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub add_syspath {
-    my $self = shift;
-    my $syspath = shift;
-
-    if(0 != udev_enumerate_add_syspath($self->{_enumerate}, $syspath)) {
-        return 0;
+    # self, syspath
+    if (0 == ($! = udev_enumerate_add_syspath($_[0]->{_enumerate}, $_[1]))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 
 sub scan_devices {
-    my $self = shift;
-
-    if(0 != udev_enumerate_scan_devices($self->{_enumerate})) {
-        return 0;
+    # self
+    if (0 == ($! = udev_enumerate_scan_devices($_[0]->{_enumerate}))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
 sub scan_subsystems {
-    my $self = shift;
-
-    if(0 != udev_enumerate_scan_subsystems($self->{_enumerate})) {
-        return 0;
+    # self
+    if (0 == ($! = udev_enumerate_scan_subsystems($self->{_enumerate}))) {
+        return 1;
     }
 
-    return 1;
+    $! = -$!;
+    return 0;
 }
 
 
@@ -212,10 +202,10 @@ Udev::FFI::Enumerate
     $enumerate->scan_devices();
     
     my @devices = $enumerate->get_list_entries();
-    for(@devices) {
+    for (@devices) {
         my $device = $udev->new_device_from_syspath($_);
     
-        if(defined $device) {
+        if (defined $device) {
             print "DEVICE: ".$device->get_sysname()."\n";
             print "VID: ".$device->get_sysattr_value('idVendor')."\n";
             print "PID: ".$device->get_sysattr_value('idProduct')."\n\n";
