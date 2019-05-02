4f79f4aa7f7d4bc3131a535b80ee415746a35515
Ilya Pavlov <ilux@cpan.org>
2019-05-02 23:21:05 +0300

    update examples

--- examples/disable_touchpad_when_usbmouse.pl
+++ examples/disable_touchpad_when_usbmouse.pl
@@ -5,97 +5,123 @@
 use strict;
 use warnings;
 
-use FindBin; # only for this example - load local Udev::FFI module
-use lib "$FindBin::Bin/../lib"; # only for this example - load local Udev::FFI module
-
 use Udev::FFI;
 
 
 use constant {
-    # add your mouse VID and PID in array
-    MOUSES => [{
-        VID => '046d',
-        PID => 'c06d'
-    }],
+    DEVICE_INPUT_OTHER          => 0,
+    DEVICE_INPUT_MOUSE          => 1,
+    DEVICE_INPUT_TOUCHPAD       => 2,
+    DEVICE_INPUT_TABLET         => 3,
+    DEVICE_INPUT_TOUCHSCREEN    => 4,
 
     ON_TOUCHPAD_COMMAND => '/usr/bin/synclient touchpadoff=0',
     OFF_TOUCHPAD_COMMAND => '/usr/bin/synclient touchpadoff=1'
 };
 
-
-my %inserted_mouses;
-
+my $udev;
 
 
-my $udev = Udev::FFI->new() or
+
+sub _get_device_type {
+    my $device = shift;
+
+    my $id_input_mouse          = $device->get_property_value('ID_INPUT_MOUSE');
+    my $id_input_touchpad       = $device->get_property_value('ID_INPUT_TOUCHPAD');
+    my $id_input_tablet         = $device->get_property_value('ID_INPUT_TABLET');
+    my $id_input_touchscreen    = $device->get_property_value('ID_INPUT_TOUCHSCREEN');
+
+    if (defined($id_input_mouse) && $id_input_mouse eq '1') {
+        # ID_INPUT_MOUSE: Touchscreens and tablets have this flag as
+        # well, since by the type of events they can produce they act as
+        # a mouse.
+        # https://askubuntu.com/questions/520359/how-to-detect-touchscreen-devices-from-a-script
+
+        if (defined($id_input_touchpad) && $id_input_touchpad eq '1') {
+            return DEVICE_INPUT_TOUCHPAD;
+        }
+        elsif (defined($id_input_tablet) && $id_input_tablet eq '1') {
+            return DEVICE_INPUT_TABLET;
+        }
+        elsif (defined($id_input_touchscreen) && $id_input_touchscreen eq '1') {
+            return DEVICE_INPUT_TOUCHSCREEN;
+        }
+
+        return DEVICE_INPUT_MOUSE;
+    }
+    elsif (defined($id_input_touchpad) && $id_input_touchpad eq '1') {
+        return DEVICE_INPUT_TOUCHPAD;
+    }
+    elsif (defined($id_input_tablet) && $id_input_tablet eq '1') {
+        return DEVICE_INPUT_TABLET;
+    }
+    elsif (defined($id_input_touchscreen) && $id_input_touchscreen eq '1') {
+        return DEVICE_INPUT_TOUCHSCREEN;
+    }
+
+
+    return DEVICE_INPUT_OTHER;
+}
+
+
+
+sub _check_for_mouse_devices {
+    my $enumerate = $udev->new_enumerate() or
+        die("Can't create enumerate context: $@");
+
+    $enumerate->add_match_subsystem('input') or
+        die("Can't add match subsystem: $!");
+
+    $enumerate->scan_devices() or
+        die("Can't scan devices: $!");
+
+    my $devices = $enumerate->get_list_entries() or
+        die "Can't get devices: $!";
+
+    for (keys(%$devices)) {
+        if (defined(my $device = $udev->new_device_from_syspath($_))) {
+            if (DEVICE_INPUT_MOUSE == _get_device_type($device)) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+
+$udev = Udev::FFI->new() or
     die "Can't create Udev::FFI object: $@.\n";
 
 
-# monitor for new devices
 my $monitor = $udev->new_monitor() or
-    die "Can't create udev monitor: $@.\n";
+    die "Can't create udev monitor: $@";
 
-$monitor->filter_by_subsystem_devtype('usb', 'usb_device');
+$monitor->filter_by_subsystem_devtype('input') or
+    die "Can't add filter to udev monitor: $!";
 
-# start monitor before enumerate to catch devices inserted between enumerate and
-# $monitor->poll()
 $monitor->start() or
-    die "Can't start udev monitor :(\n";
+    die "Can't start udev monitor: $!";
 
 
-# check already inserted devices
-my $enumerate = $udev->new_enumerate() or
-    die "Can't create enumerate context: $@.\n";
-
-$enumerate->add_match_subsystem('usb');
-
-$enumerate->add_match_sysattr('idVendor');
-$enumerate->add_match_sysattr('idProduct');
-
-$enumerate->scan_devices();
-
-my @inserted_devices = $enumerate->get_list_entries();
-for(@inserted_devices) {
-    my $device = $udev->new_device_from_syspath($_);
-    my $device_vid = $device->get_sysattr_value("idVendor");
-    my $device_pid = $device->get_sysattr_value("idProduct");
-
-    for(@{+MOUSES}) {
-        if($device_vid eq $_->{VID} && $device_pid eq $_->{PID}) {
-            $inserted_mouses{ $device->get_devpath() } = 1;
-            last;
-        }
-    }
-}
-
-# known mouses > 0
-if(%inserted_mouses) {
+if (1 == _check_for_mouse_devices()) {
     system(OFF_TOUCHPAD_COMMAND);
+}
+else {
+    system(ON_TOUCHPAD_COMMAND);
 }
 
 
 for(;;) {
     my $device = $monitor->poll(); # blocking read
     my $action = $device->get_action();
-    my $device_vid = $device->get_sysattr_value("idVendor");
-    my $device_pid = $device->get_sysattr_value("idProduct");
 
-    if($action eq 'add' && defined($device_vid) && defined($device_pid)) {
-        for(@{+MOUSES}) {
-            if($device_vid eq $_->{VID} && $device_pid eq $_->{PID}) {
-                system(OFF_TOUCHPAD_COMMAND)
-                    unless %inserted_mouses;
-
-                $inserted_mouses{ $device->get_devpath() } = 1;
-                last;
-            }
+    if($action eq 'add' || $action eq 'remove') {
+        if (1 == _check_for_mouse_devices()) {
+            system(OFF_TOUCHPAD_COMMAND);
         }
-    }
-    elsif($action eq 'remove') {
-        delete $inserted_mouses{ $device->get_devpath() };
-
-        # known mouses == 0
-        unless(%inserted_mouses) {
+        else {
             system(ON_TOUCHPAD_COMMAND);
         }
     }
